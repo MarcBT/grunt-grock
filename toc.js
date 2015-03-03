b8b1694b4be52810aa7bddab2514f0a319837f54
Marc Beuret <marc@lacompany.fr>
2015-03-03 23:15:57 +0100

    gh-pages initial commit -> doc generated via grunt-grock task

---
+++ toc.js
@@ -0,0 +1,65 @@
+window.files = [
+{
+  "path": "README.md.html",
+  "originalName": "README.md",
+  "originalPath": "README.md",
+  "name": "README.md.html",
+  "title": "grunt-grock",
+  "toc": [
+    {
+      "level": 1,
+      "slug": "grunt-grock",
+      "title": "grunt-grock"
+    },
+    {
+      "level": 2,
+      "slug": "getting-started",
+      "title": "Getting Started"
+    },
+    {
+      "level": 2,
+      "slug": "the-quot-grock-quot-task",
+      "title": "The &quot;grock&quot; task"
+    },
+    {
+      "level": 3,
+      "slug": "usage",
+      "title": "Usage"
+    },
+    {
+      "level": 3,
+      "slug": "options",
+      "title": "Options"
+    },
+    {
+      "level": 2,
+      "slug": "contributing",
+      "title": "Contributing"
+    }
+  ]
+},
+{
+  "path": "index.html",
+  "originalName": "Gruntfile.js",
+  "originalPath": "Gruntfile.js",
+  "name": "index.html",
+  "lang": "javascript",
+  "toc": []
+},
+{
+  "path": "tasks/grock.js.html",
+  "originalName": "grock.js",
+  "originalPath": "tasks/grock.js",
+  "name": "grock.js.html",
+  "lang": "javascript",
+  "toc": []
+},
+{
+  "path": "test/grock_test.js.html",
+  "originalName": "grock_test.js",
+  "originalPath": "test/grock_test.js",
+  "name": "grock_test.js.html",
+  "lang": "javascript",
+  "toc": []
+}
+];
