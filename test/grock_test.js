958e91fc684ccc7f05e18827060cd67f8594a785
Marc Beuret <marcbeuret@yahoo.fr>
2015-03-31 23:21:39 +0200

    Add latest grock options "extScripts and extStyles" : add test

--- test/grock_test.js
+++ test/grock_test.js
@@ -1,8 +1,3 @@
-'use strict';
-
-var grunt = require('grunt');
-var gf = grunt.file;
-
 /*
   ======== A Handy Little Nodeunit Reference ========
   https://github.com/caolan/nodeunit
@@ -23,30 +18,54 @@ var gf = grunt.file;
     test.ifError(value)
 */
 
+'use strict';
+
+var grunt = require('grunt'),
+  path = require('path'),
+  fs = require('fs'),
+  gf = grunt.file;
+
 exports.grock = {
-  setUp: function(done) {
+  setUp: function (done) {
     // setup here if necessary
     done();
   },
-  custom_options: function(test) {
+  custom_options: function (test) {
     // Tests :
     // Check if Folder 'out' has been created
     // Checks if documentation files have been created
-    
-    var config = grunt.config.get('grock');
-    var outFolder = config.options.out;
-    var files = gf.expand(config.files);
-    var index = outFolder + '/index.html';
-        
+
+    var config = grunt.config.get('grock').main,
+      outFolder = config.options.out,
+      files = gf.expand(config.src),
+      index = outFolder + '/index.html';
+
     test.expect(1 + files.length);
-        
-    test.ok(grunt.file.exists(outFolder+ '/toc.js'), 'Output folder not created or empty');
-    for (var i = 0; i<files.length; i++){
+
+    test.ok(grunt.file.exists(outFolder + '/toc.js'), 'Output folder not created or empty');
+    for (var i = 0; i < files.length; i++) {
       var outFile = outFolder + '/' + files[i].split('./')[1] + '.html';
       test.ok(gf.exists(outFile) || gf.exists(index), 'File ' + outFile + ' not created');
     }
-        
+
     test.done();
 
+  },
+  externals: function (test) {
+    // Tests :
+    // Check if external styles have been inlined into doc   files
+
+    var options = grunt.config.get('grock').externals.options,
+        externals = gf.expand(options.extScripts).concat(gf.expand(options.extStyles)),
+        index = gf.read(path.join(__dirname, '..', options.out, 'index.html'));
+
+    test.expect(externals.length);
+    
+    externals.forEach(function(file) {
+      var extraContent = gf.read(file);
+      test.ok( (index.indexOf(extraContent) > -1), 'Content of external file '+file+ 'has not been inlined');
+    });
+    
+    test.done();
   }
 };
