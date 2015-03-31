80c22ef6bcd48f1fe1d81b09e040c9954686cb88
Marc Beuret <marcbeuret@yahoo.fr>
2015-03-31 23:21:26 +0200

    Add latest grock options "extScripts and extStyles" : update task

--- tasks/grock.js
+++ tasks/grock.js
@@ -13,13 +13,18 @@ module.exports = function(grunt) {
   // Please see the Grunt documentation for more information regarding task
   // creation: http://gruntjs.com/creating-tasks
 
-  var grock = require('grock').generator;
+  var grock = require('grock').generator,
+      gf    = require('grunt').file;
 
   grunt.registerMultiTask('grock', 'A simple grunt task to generate a project\'s documentation using Grock.', function() {
-    var done = this.async(),
+    var done    = this.async(),
         options = this.options(),
-        glob = (this.filesSrc && this.filesSrc.length > 0) ? this.filesSrc : 'lib/*.coffee';
-
+        glob    = (this.filesSrc && this.filesSrc.length > 0) ? this.filesSrc : 'lib/*.coffee';
+    
+    // Prepare ext-scripts and ext-styles
+    var extScripts = (options.extScripts) ? gf.expand(options.extScripts) : false;
+    var extStyles  = (options.extStyles) ? gf.expand(options.extStyles) : false;
+    
     var args = {
       'glob': glob,
       'out': options.out || 'docs/',
@@ -28,9 +33,11 @@ module.exports = function(grunt) {
       'index': options.index ||'Readme.md',
       'root': options.root || '.',
       'whitespace-after-token': options.whitespaceAfterToken || false,
+      'ext-scripts': extScripts,
+      'ext-styles': extStyles,
       'start': process.hrtime()
     };
-
+    
     grock(args)
       .then(function () { done(); }) // doc generation successful
       .then(null, function () { done(false); }); // there was an error;
